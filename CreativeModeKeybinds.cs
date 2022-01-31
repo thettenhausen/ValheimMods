c67deec512fc0e2cc07a76955cb6237384dd5c35
Thomas <thomas@hettenhausen.net>
2022-01-31 18:35:48 +0100

    Add files via upload

---
+++ CreativeModeKeybinds.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+
+namespace CreativeModeKeybinds
+{
+    [BepInPlugin("com.github.hettenhausen.valheim.CreativeModeKeybinds", "CreativeModeKeybinds", "0.0.1")]
+    [BepInProcess("valheim.exe")]
+    public class CreativeModeKeybinds : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("com.github.hettenhausen.valheim.CreativeModeKeybinds");
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Player), "Update")]
+    public class Player_Patch
+    {
+        static void Postfix(ref Player __instance)//, ref bool __flag)
+        {
+            //if (__flag)
+            //{
+                if (Player.m_debugMode && Console.instance.IsCheatsEnabled())
+                {
+                    if (Input.GetKeyDown(KeyCode.Keypad7)) // was KeyCode.Z
+                    {
+                        __instance.ToggleDebugFly();
+                    }
+                    if (Input.GetKeyDown(KeyCode.Keypad8)) // was KeyCode.B
+                    {
+                        __instance.ToggleNoPlacementCost();
+                    }
+                }
+            //}
+        }
+    }
+}
+
